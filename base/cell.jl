6c4987d34574f0f4236da17ad428668539569a8f
Toivo Henningsson <toivo.h.h@gmail.com>
2012-04-19 22:19:51 +0200

    tidied up

--- base/cell.jl
+++ base/cell.jl
@@ -37,16 +37,10 @@ end
 
 # map cell array
 map(f, a::Array{Any,1}) = { f(a[i]) | i=1:length(a) }
-# map(f, a::Array{Any,1}, b::Array{Any,1}) =
-#     { f(a[i],b[i]) | i=1:min(length(a),length(b)) }
-# function map(f, as::Array{Any,1}...)
-#     n = min(length, as)
-#     { f(map(a->a[i],as)...) | i=1:n }
-# end
 map(f, a::Array{Any,1}, b::Array{Any,1}) =
-    { f(a[i],b[i]) | i=1:__map_checked_common_length((a, b)) }
+    { f(a[i],b[i]) | i=1:length_checked_equal(a, b) }
 function map(f, as::Array{Any,1}...)
-    n = __map_checked_common_length(as)
+    n = length_checked_equal(as...)
     { f(map(a->a[i],as)...) | i=1:n }
 end
 
