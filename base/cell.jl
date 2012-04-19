e48851afda8e186983798076623a7bbdb0c931c2
Toivo Henningsson <toivo.h.h@gmail.com>
2012-04-19 14:55:48 +0200

    Added common length check to cell.jl

--- base/cell.jl
+++ base/cell.jl
@@ -37,10 +37,16 @@ end
 
 # map cell array
 map(f, a::Array{Any,1}) = { f(a[i]) | i=1:length(a) }
+# map(f, a::Array{Any,1}, b::Array{Any,1}) =
+#     { f(a[i],b[i]) | i=1:min(length(a),length(b)) }
+# function map(f, as::Array{Any,1}...)
+#     n = min(length, as)
+#     { f(map(a->a[i],as)...) | i=1:n }
+# end
 map(f, a::Array{Any,1}, b::Array{Any,1}) =
-    { f(a[i],b[i]) | i=1:min(length(a),length(b)) }
+    { f(a[i],b[i]) | i=1:__map_checked_common_length((a, b)) }
 function map(f, as::Array{Any,1}...)
-    n = min(length, as)
+    n = __map_checked_common_length(as)
     { f(map(a->a[i],as)...) | i=1:n }
 end
 
